06cd7c0cee6b52f6533b5177fd6bf76a9042322b
nabin-bh <nabinbhandari104@gmail.com>
2023-09-07 19:06:40 +0545

    creating first migroservice send-email route gin

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package microservicernd
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/send-email", func(c *gin.Context) {
+		//
+	})
+
+	r.Run(":8080")
+}
